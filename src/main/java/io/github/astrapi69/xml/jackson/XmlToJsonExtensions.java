08edcb6e15df2810a95de15b18310063c05bae43
astrapi69 <asterios.raptis@gmx.net>
2023-03-03 17:19:02 +0100

    new converter class XmlToJsonExtensions created

---
+++ src/main/java/io/github/astrapi69/xml/jackson/XmlToJsonExtensions.java
@@ -0,0 +1,58 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2022 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.astrapi69.xml.jackson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+
+/**
+ * The class {@link XmlToJsonExtensions} provides methods for convert xml string objects to json
+ * string
+ */
+public class XmlToJsonExtensions
+{
+
+	/**
+	 * Creates from the given xml string and class a json string
+	 *
+	 * @param <T>
+	 *            the generic type of the return type
+	 * @param xmlString
+	 *            the xml
+	 * @param clazz
+	 *            the class of the generic type
+	 * @return the json string
+	 * @throws JsonProcessingException
+	 *             is thrown when processing json content that are not pure I/O problems
+	 */
+	public static <T> String toJson(final @NonNull String xmlString, final @NonNull Class<T> clazz)
+		throws JsonProcessingException
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(XmlToObjectExtensions.toObject(xmlString, clazz));
+	}
+
+}
